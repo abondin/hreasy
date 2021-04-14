934bbe03e0c3a9098fb136da19f61c00a72e780b
Alexander Bondin <Alexander.Bondin@stm-labs.ru>
2021-04-14 19:35:15 +0300

    Fix comments

--- platform/src/main/java/ru/abondin/hreasy/platform/repo/employee/EmployeeDetailedEntry.java
+++ platform/src/main/java/ru/abondin/hreasy/platform/repo/employee/EmployeeDetailedEntry.java
@@ -2,6 +2,7 @@ package ru.abondin.hreasy.platform.repo.employee;
 
 import lombok.Data;
 import org.springframework.data.relational.core.mapping.Table;
+import ru.abondin.hreasy.platform.service.skills.dto.RatingsMapper;
 
 @Data
 @Table("v_employee_detailed")
@@ -12,20 +13,11 @@ public class EmployeeDetailedEntry extends EmployeeEntry {
     private String levelName;
     private String currentProjectName;
     private String officeLocationName;
+
     /**
-     * Joined to one String information about all skills with average rating for each skill.
-     * <p>The format is: SKILL_1_ID|$|SKILL_1_NAME|$|G_1_ID|$|G_1_NAME|$|AVG|$|CNT|#|SKILL_2_ID|$|SKILL_2_NAME|$|G_2_ID|$|G_2_NAME|$|AVG|$|CNT|#|...</p>
-     * <ul>
-     *     <li>SKILL_1_ID, SKILL_2_ID - employees skill ids</li>
-     *     <li>SKILL_1_NAME, SKILL_2_NAME - employees skill names</li>
-     *     <li>G_1_ID, G_2_ID - group id if skill</li>
-     *     <li>G_1_NAME, G_2_NAME - group name if skill</li>
-     *     <li>AVG - skill average rating</li>
-     *     <li>CNT - ratings count (everyone can rating your skill)</li>
-     *     <li>|$| - delimiter between elements of one skill</li>
-     *     <li>|#| - delimiter between skills</li>
-     * </ul>
-     * <p>Example: Java|$|1|$|5|#|Java Script|$|0|$|0|#|Python|$|2|$|4.5</p>
+     * All employee skills with ratings assembled to string
+     *
+     * @see RatingsMapper#parseAssembledSkills
      */
     private String aggregatedSkills;
 }
