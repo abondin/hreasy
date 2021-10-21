eba81dafa773f27ddfc99ecc28fd035b105f8d7a
Alexander Bondin <Alexander.Bondin@stm-labs.ru>
2021-10-21 10:25:18 +0300

    Temp employee export commit. Nothing works

---
+++ platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/AdminEmployeeExcelExporter.java
@@ -0,0 +1,57 @@
+package ru.abondin.hreasy.platform.service.admin.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import ru.abondin.hreasy.platform.service.admin.employee.dto.EmployeeExportDto;
+import ru.abondin.hreasy.platform.service.dto.SimpleDictDto;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Export all information about employees as an excel formatted table
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class AdminEmployeeExcelExporter {
+
+    @Value("${classpath:jxls/admin_employees_template.xlsx}")
+    private Resource template;
+
+    @Data
+    @Builder
+    public static class AdminEmployeeExportBundle {
+        private String exportedBy;
+        private OffsetDateTime exportTime;
+        private List<SimpleDictDto> allProjects;
+        private List<SimpleDictDto> allDepartments;
+        private List<SimpleDictDto> allPositions;
+        private List<SimpleDictDto> allLevels;
+        private List<SimpleDictDto> allLocations;
+        private List<EmployeeExportDto> employees;
+        private Locale locale;
+    }
+
+
+    public void exportEmployees(AdminEmployeeExportBundle bundle, OutputStream out) throws IOException {
+        try (var is = template.getInputStream()) {
+            var context = new Context();
+            context.putVar("employees", bundle.getEmployees());
+            context.putVar("exportedAt", bundle.getExportTime().toLocalDateTime());
+            JxlsHelper.getInstance().processTemplate(is, out, context);
+        }
+    }
+}
