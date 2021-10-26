9c4e7f312570666d882226b531964f6b77c63c8a
Alexander Bondin <Alexander.Bondin@stm-labs.ru>
2021-10-26 18:06:21 +0300

    Add all dictionaries to employees excel export

--- platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/AdminEmployeeExportService.java
+++ platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/AdminEmployeeExportService.java
@@ -11,11 +11,15 @@ import ru.abondin.hreasy.platform.auth.AuthContext;
 import ru.abondin.hreasy.platform.service.DateTimeService;
 import ru.abondin.hreasy.platform.service.DictService;
 import ru.abondin.hreasy.platform.service.admin.employee.dto.EmployeeAllFieldsMapper;
+import ru.abondin.hreasy.platform.service.dto.SimpleDictDto;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,25 +33,37 @@ public class AdminEmployeeExportService {
 
     public Mono<Resource> export(AuthContext auth, Locale locale) {
         final OffsetDateTime now = dateTimeService.now();
-        return employeeService.findAll(auth)
-                .map(e -> {
-                    //TODO Populate dictionaries
-                    var emplExp = mapper.toExportWithoutDictanories(e);
-                    emplExp.setPosition(null);
-                    emplExp.setLevel(null);
-                    emplExp.setOfficeLocation(null);
-                    emplExp.setDepartment(null);
-                    emplExp.setCurrentProject(null);
-                    return emplExp;
-                }).collectList().map(emplList ->
-                        AdminEmployeeExcelExporter.AdminEmployeeExportBundle.builder()
-                                .exportTime(now)
-                                .exportedBy(auth.getUsername())
-                                .locale(locale)
-                                .employees(emplList)
-                                .build()
-                )
-                .flatMap(bundle -> export(bundle));
+        return Mono.zip(
+                dictService.findProjects(auth).collectList()
+                , dictService.findDepartments(auth).collectList()
+                , dictService.findLevels(auth).collectList()
+                , dictService.findOfficeLocations(auth).collectList()
+                , dictService.findPositions(auth).collectList()
+        ).flatMap(tupple -> {
+            var projects = fromDicts(tupple.getT1());
+            var departments = fromDicts(tupple.getT2());
+            var levels = fromDicts(tupple.getT3());
+            var officeLocations = fromDicts(tupple.getT4());
+            var positions = fromDicts(tupple.getT5());
+            return employeeService.findAll(auth)
+                    .map(e -> {
+                        var emplExp = mapper.toExportWithoutDictanories(e);
+                        emplExp.setCurrentProject(projects.get(e.getCurrentProjectId()));
+                        emplExp.setDepartment(departments.get(e.getDepartmentId()));
+                        emplExp.setPosition(positions.get(e.getPositionId()));
+                        emplExp.setLevel(levels.get(e.getLevelId()));
+                        emplExp.setOfficeLocation(officeLocations.get(e.getOfficeLocationId()));
+                        return emplExp;
+                    }).collectList().map(emplList ->
+                            AdminEmployeeExcelExporter.AdminEmployeeExportBundle.builder()
+                                    .exportTime(now)
+                                    .exportedBy(auth.getUsername())
+                                    .locale(locale)
+                                    .employees(emplList)
+                                    .build()
+                    )
+                    .flatMap(bundle -> export(bundle));
+        });
     }
 
 
@@ -60,5 +76,9 @@ public class AdminEmployeeExportService {
             log.error("Unable to generate employees export report document", e);
             return Mono.error(new BusinessError("errors.export"));
         }
+    }
+
+    private Map<Integer, String> fromDicts(List<SimpleDictDto> list) {
+        return list.stream().collect(Collectors.toMap(SimpleDictDto::getId, SimpleDictDto::getName));
     }
 }
