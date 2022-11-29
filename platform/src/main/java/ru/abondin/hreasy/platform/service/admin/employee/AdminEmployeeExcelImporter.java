66ff5cc6a741e49cae1005fb8dec7ceba3d96fe3
Alexander Bondin <Alexander.Bondin@stm-labs.ru>
2022-11-29 18:02:29 +0300

    Temp commit. Nothing works

--- platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/AdminEmployeeExcelImporter.java
+++ platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/AdminEmployeeExcelImporter.java
@@ -1,40 +1,42 @@
 package ru.abondin.hreasy.platform.service.admin.employee;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jxls.common.Context;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.jxls.reader.ReaderBuilder;
-import org.jxls.reader.XLSReader;
-import org.jxls.util.JxlsHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.xml.sax.SAXException;
 import reactor.core.publisher.Flux;
-import ru.abondin.hreasy.platform.service.admin.employee.dto.CreateOrUpdateEmployeeBody;
-import ru.abondin.hreasy.platform.service.admin.employee.dto.EmployeeExportDto;
+import ru.abondin.hreasy.platform.service.admin.employee.dto.ImportEmployeeExcelDto;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Create or update employees from external excel file
  */
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AdminEmployeeExcelImporter {
 
     @Value("${classpath:jxls/import_employees_template.xml}")
-    private Resource template;
+    private final Resource template;
 
-    public Flux<CreateOrUpdateEmployeeBody> importEmployees(InputStream excel) throws IOException {
-        ReaderBuilder.buildFromXML()
-        return null ;
+    public Flux<ImportEmployeeExcelDto> importEmployees(InputStream excel) throws IOException {
+        try {
+            var employees = new ArrayList<ImportEmployeeExcelDto>();
+            var beans = new HashMap<String, Object>();
+            beans.put("employees", employees);
+            var readStatus = ReaderBuilder.buildFromXML(template.getInputStream()).read(excel, beans);
+            log.info("Import employee: Read excel file using template status {}: {}", readStatus.isStatusOK(), readStatus.getReadMessages());
+            return Flux.fromIterable(employees);
+        } catch (InvalidFormatException | SAXException e) {
+            throw new IOException(e);
+        }
     }
 }
--- platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/dto/ImportEmployeeExcelDto.java
+++ platform/src/main/java/ru/abondin/hreasy/platform/service/admin/employee/dto/ImportEmployeeExcelDto.java
@@ -5,4 +5,6 @@ import lombok.Data;
 
 @Data
 public class ImportEmployeeExcelDto {
+    private String displayName;
+    private String email;
 }
