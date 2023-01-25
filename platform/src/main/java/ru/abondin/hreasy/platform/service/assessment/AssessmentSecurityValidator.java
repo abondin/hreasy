31b30095cb5c2cadff9c72968d0b585c85591b28
Alexander Bondin <Alexander.Bondin@stm-labs.ru>
2023-01-25 11:56:12 +0300

    Allow project/ba manager to see full assessment information

--- platform/src/main/java/ru/abondin/hreasy/platform/service/assessment/AssessmentSecurityValidator.java
+++ platform/src/main/java/ru/abondin/hreasy/platform/service/assessment/AssessmentSecurityValidator.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import ru.abondin.hreasy.platform.auth.AuthContext;
+import ru.abondin.hreasy.platform.sec.ProjectHierarchyAccessor;
 
 import java.util.List;
 
@@ -17,6 +18,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AssessmentSecurityValidator {
 
+    private final ProjectHierarchyAccessor hierarchyAccessor;
+
     public Mono<Boolean> validateCanCreateAssessment(AuthContext auth) {
         return Mono.defer(() -> {
             if (auth.getAuthorities().contains("create_assessment")) {
@@ -27,17 +30,23 @@ public class AssessmentSecurityValidator {
         });
     }
 
-    public Mono<Boolean> validateOwnerOrCanViewAssessmentFull(AuthContext auth, List<Integer> assessmentOwners) {
+    public Mono<Boolean> validateOwnerOrCanViewAssessmentFull(AuthContext auth, int employee, List<Integer> assessmentOwners) {
         return Mono.defer(() -> {
-            if (auth.getAuthorities().contains("view_assessment_full")) {
-                return Mono.just(true);
-            } else if (assessmentOwners.contains(auth.getEmployeeInfo().getEmployeeId())) {
+                    if (auth.getAuthorities().contains("view_assessment_full")) {
+                        return Mono.just(true);
+                    } else if (assessmentOwners.contains(auth.getEmployeeInfo().getEmployeeId())) {
+                        return Mono.just(true);
+                    } else {
+                        return hierarchyAccessor.isManager(auth, employee);
+                    }
+                }
+        ).flatMap(v -> {
+            if (v) {
                 return Mono.just(true);
             }
             return Mono.error(new AccessDeniedException("Only user with permission view_assessment_full" +
                     " or assessment owner can see detailed information and update assessment"));
         });
-
     }
 
     public Mono<Boolean> validateExportAssessments(AuthContext auth) {
