98ab857124c73e4736e859454fb4f8830f1808d2
Alexander Bondin <Alexander.Bondin@stm-labs.ru>
2023-02-03 17:26:43 +0300

    Tmp

---
+++ platform/src/main/java/ru/abondin/hreasy/platform/api/ts/TimesheetController.java
@@ -0,0 +1,42 @@
+package ru.abondin.hreasy.platform.api.ts;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.abondin.hreasy.platform.auth.AuthHandler;
+import ru.abondin.hreasy.platform.service.ts.TimesheetService;
+import ru.abondin.hreasy.platform.service.ts.dto.TimesheetAggregatedFilter;
+import ru.abondin.hreasy.platform.service.ts.dto.TimesheetReportBody;
+import ru.abondin.hreasy.platform.service.ts.dto.TimesheetSummaryDto;
+
+import java.time.LocalDate;
+
+@RestController()
+@RequestMapping("/api/v1/timesheet")
+@RequiredArgsConstructor
+@Slf4j
+public class TimesheetController {
+
+    private final TimesheetService service;
+
+    @PostMapping("/{employeeId}")
+    public Mono<Integer> report(@PathVariable int employeeId, @RequestBody TimesheetReportBody body) {
+        return AuthHandler.currentAuth().flatMap(
+                auth -> service.report(auth, employeeId, body));
+    }
+
+    @DeleteMapping("/{employeeId}/{recordId}")
+    public Mono<Integer> delete(Integer employeeId, Integer recordId) {
+        return AuthHandler.currentAuth().flatMap(
+                auth -> service.delete(auth, employeeId, recordId));
+    }
+
+    @GetMapping("/")
+    public Flux<TimesheetSummaryDto> timesheetSummary(@RequestParam LocalDate from, @RequestParam LocalDate to) {
+        return AuthHandler.currentAuth().flatMapMany(
+                auth -> service.timesheetSummary(auth, TimesheetAggregatedFilter.builder().from(from).to(to).build()));
+    }
+
+}
